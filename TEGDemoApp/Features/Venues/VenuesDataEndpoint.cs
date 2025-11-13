3d429db90f40b1fddbb0e2eca6e0f2d88fb8aad6
Gaurav Misra <gaurmis@otmail.com>
2025-11-13 19:23:37 +1100

    Added local exception handling and updates to ReadMe file

--- TEGDemoApp/Features/Venues/VenuesDataEndpoint.cs
+++ TEGDemoApp/Features/Venues/VenuesDataEndpoint.cs
@@ -6,19 +6,35 @@
         {
             app.MapGet("/api/venues", async (IVenueApiClient client) =>
             {
-                var venues = await client.GetAllVenuesAsync();
-
-                if (venues == null)
+                try
                 {
-                    return Results.BadRequest();
-                }
+                    var venues = await client.GetAllVenuesAsync();
 
-                if (venues.Count == 0) 
-                {
-                    return Results.NotFound();
+                    if (venues == null)
+                    {
+                        return Results.Problem(
+                            detail: "Bad Data",
+                            title: "Bad Request",
+                            statusCode: 400);
+                    }
+
+                    if (venues.Count == 0)
+                    {
+                        return Results.Problem(
+                            detail: "No Venue was found",
+                            title: "Venues not found",
+                            statusCode: 404);
+                    }
+
+                    return Results.Ok(venues);
                 }
-                
-                return Results.Ok(venues);
+                catch (Exception ex)
+                {
+                    return Results.Problem(
+                            detail: "An error has occurred",
+                            title: "Internal Server Error",
+                            statusCode: 500);
+                }
             });
         }
     }
--- TEGDemoApp/ReadMe.md
+++ TEGDemoApp/ReadMe.md
@@ -1,1 +1,2 @@
-
+Refer Readme.md in main repo folder "TEGDemo" for back-end details.
+Refer Readme.md in the "TEGDemo/TEGDemoApp/eventsapp" folder for details on the frontend React app.
